8e31e65bdff4e37360c1dfa866614ad6ce786e03
MIRE.LEZEA <MIRE.LEZEA@7529c1be-fc49-0410-8409-bd4ad1899306>
2008-04-08 15:06:20 +0000

    cliente.java sortu dut eta clienteKud hasi naiz baina bukatu gabe dago.

--- Buenetxea/src/buenetxea/kudeatzaileak/ClienteKud.java
+++ Buenetxea/src/buenetxea/kudeatzaileak/ClienteKud.java
@@ -1,5 +1,27 @@
 package buenetxea.kudeatzaileak;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Statement;
+
+import buenetxea.db.Connector;
+import buenetxea.objektuak.Cliente;
 public class ClienteKud {
 
+	private final Connection connection;
+	private final Statement statement;
+	
+	public ClienteKud(Connector conector) {
+		
+		this.connection=conector.getConnection();
+		this.statement=conector.getStatement();
+	}
+	
+	 Cliente getCliente (int dni) throws IOException
+	{
+		return null;
+	}
+	
+	
+	
 }
